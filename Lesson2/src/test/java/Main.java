e584f8d6f147cd0f94475632baabcdbaa8595cad
Sebastian Targosz <stargosz@sii.pl>
2021-04-23 11:40:34 +0200

    Task-Lesson2

---
+++ Lesson2/src/test/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TokenGenerator {
+
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+
+        int tokenLength;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This program will generate token of length 5/10/15 random characters. Please provide one of them: ");
+        tokenLength = scanner.nextInt();
+        switch (tokenLength) {
+            case 5:
+            case 15:
+            case 10:
+                System.out.println("Token: " + tokenGenerator(tokenLength));
+                break;
+            default:
+                System.out.println("Please enter correct value, 5, 10 or 15");
+        }
+    }
+
+    public static String tokenGenerator(int length) {
+
+        String Token = "abcdefghijklmnopqrstuvxy" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "!@#$%^&*()";
+        StringBuilder sB = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomChar = random.nextInt(Token.length());
+            char rndChar = Token.charAt(randomChar);
+
+            sB.append(rndChar);
+        }
+        return sB.toString();
+    }
+}
